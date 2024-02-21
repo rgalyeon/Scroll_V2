178f54330df491fbb7d99bd9be02887300e5c120
rgalyeon <dmifilin@gmail.com>
2024-02-22 01:20:47 +0300

    Add: eip

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ USE_PROXY = True
 SLEEP_FROM = 1500  # Second
 SLEEP_TO = 3600  # Second
 
-QUANTITY_THREADS = 3
+QUANTITY_THREADS = 50
 
 THREAD_SLEEP_FROM = 30
 THREAD_SLEEP_TO = 50
@@ -22,8 +22,8 @@ REALTIME_GWEI = True  # if true - you can change gwei while program is working
 RANDOMIZE_GWEI = True  # if True, max Gwei will be randomized for each wallet for each transaction
 MAX_GWEI_RANGE = [15, 18]
 
-GAS_SLEEP_FROM = 24000
-GAS_SLEEP_TO = 34000
+GAS_SLEEP_FROM = 10
+GAS_SLEEP_TO = 20
 
 MAX_PRIORITY_FEE = {
     "ethereum": 0.01,
@@ -31,6 +31,7 @@ MAX_PRIORITY_FEE = {
     "arbitrum": 0.1,
     "base": 0.001,
     "zksync": 0.25,
+    "optimism": 0.01
 }
 
 GAS_MULTIPLIER = 1.3
